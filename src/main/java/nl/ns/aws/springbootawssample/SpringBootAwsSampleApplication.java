6617a0267dd8be42f9c00c10ad2ba6418806af84
Albert van 't Hart <avthart@gmail.com>
2019-08-07 09:10:36 +0200

    Initial Commit

---
+++ src/main/java/nl/ns/aws/springbootawssample/SpringBootAwsSampleApplication.java
@@ -0,0 +1,13 @@
+package nl.ns.aws.springbootawssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/ns/aws/springbootawssample/SpringBootAwsSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.ns.aws.springbootawssample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAwsSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
